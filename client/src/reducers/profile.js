5146dfb67789038c029430a74660fcbe8a4dcaa2
jmjuanico <joey.juanico@gmail.com>
2020-04-16 21:15:43 +0800

    adding education and experience

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -2,6 +2,7 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE,
+  UPDATE_PROFILE,
 } from '../actions/types';
 
 const initialState = {
@@ -15,6 +16,7 @@ const initialState = {
 export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
+    case UPDATE_PROFILE:
     case GET_PROFILE:
       return {
         ...state,
